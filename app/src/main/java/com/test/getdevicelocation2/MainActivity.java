63d6db834b2d891803bb053cb082049af2bc742a
VeronikaRevjakina <veronikaw777@gmail.com>
2019-04-05 22:08:23 +0300

    Working connect with http,another url

--- app/src/main/java/com/test/getdevicelocation2/MainActivity.java
+++ app/src/main/java/com/test/getdevicelocation2/MainActivity.java
@@ -2,6 +2,7 @@ package com.test.getdevicelocation2;
 
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,12 +14,19 @@ import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
+
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
 public class MainActivity extends AppCompatActivity {
 
     private FusedLocationProviderClient client;
-
+    String contentText = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +50,14 @@ public class MainActivity extends AppCompatActivity {
                      if(location !=null) {
                          TextView textView = findViewById(R.id.location);
                          textView.setText(location.toString());
+
+                         double elevation;
+
+                         //String url = "https://maps.googleapis.com/maps/api/elevation/json?locations=" + Lat + "," + Lon + "&key="+ApiKey;
+                         String url="https://developer.android.com/index.html";
+                         if(contentText==null){
+                             new ProgressTask().execute(url);
+                         }
                      }
                     }
                 });
@@ -50,5 +66,43 @@ public class MainActivity extends AppCompatActivity {
     }
     private void requestPermission(){
        ActivityCompat.requestPermissions(this,new String[] {ACCESS_FINE_LOCATION},1);
+    }
+
+    private class ProgressTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... path) {
+            String content;
+            try{
+                content = getContent(path[0]);
+            }
+            catch (IOException ex){
+                content = ex.getMessage();
+            }
+
+            return content;
+        }
+
+        private String getContent(String path) throws IOException {
+            BufferedReader reader = null;
+            try {
+                URL url = new URL(path);
+                HttpsURLConnection c = (HttpsURLConnection) url.openConnection();
+                c.setRequestMethod("GET");
+                c.setReadTimeout(10000);
+                c.connect();
+                reader = new BufferedReader(new InputStreamReader(c.getInputStream()));
+                StringBuilder buf = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    buf.append(line + "\n");
+                }
+                return (buf.toString());
+            } finally {
+                if (reader != null) {
+                    reader.close();
+                }
+            }
+        }
+
     }
 }
